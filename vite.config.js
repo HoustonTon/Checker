42917100ea560be2db2106156fcecb6b24f7b69c
HoustonTon <ghghuu4570@gmail.com>
2024-12-03 02:33:04 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/src': resolve(__dirname, 'src')
+    }
+  },
   optimizeDeps: {
     include: ['pdfjs-dist']
   },
-  css: {
-    postcss: './postcss.config.js'
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
   }
 });
